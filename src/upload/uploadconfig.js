a59271e041a082e9f23d960e7072b8f8afc327cb
Mariana Cardoso <mariana-card@outlook.com>
2024-10-13 14:47:41 -0300

    Pos Graduação - Recursos avançados

---
+++ src/upload/uploadconfig.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const crypto = require('crypto');
+const multer = require('multer');
+
+const tempDir = path.resolve(__dirname, '..','..','imagens');
+
+module.exports = {
+    directory: tempDir,
+    storage: multer.diskStorage({
+        destionation: tempDir,
+        filename(request, file, callback){
+            const nomeHash = crypto.randomBytes(10).toString('HEX');
+            const nomeArquivo = `${nomeHash}-${file.originalname}`;
+
+            return callback(null, nomeArquivo);
+        }
+    })
+};
